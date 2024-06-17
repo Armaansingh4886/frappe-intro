8dc0f89139e7cc80f175051555900a3b27fcb3da
armaansingh4886 <armaansingh4886@gmail.com>
2024-06-17 17:18:23 +0530

    filtering code

--- my_app/doctype/course/course.js
+++ my_app/doctype/course/course.js
@@ -1,16 +1,10 @@
 // Copyright (c) 2024, abc and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Course", {
-	refresh(frm) {
-                frappe.call({
-                        method:'my_app.my_app.doctype.course.course.get_all_courses',
-                        callback: function (res){
-                                console.log(res);
-                        }
-                })
-        
-	},
-});
+// frappe.ui.form.on("Course", {
+// 	refresh(frm) {
+              
+// 	},
+// });
 
 
